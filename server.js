1fd01efbc01d6030547628a7dce6449f33da5ae2
Jennifer Sieber <jenwebdev2@gmail.com>
2023-10-02 12:38:12 -0600

    part two-refactor to mvc

--- server.js
+++ server.js
@@ -6,18 +6,15 @@ const app = express()
 require('dotenv').config({path: '.env'})
 const PORT = 3000
 
-// middleware so app knows how to use JSON
+
 app.use(express.json())
-// middleware so app knows how to use formurlencoded
 app.use(express.urlencoded({ extended: false }))
 
 
-// Route to see server in browser
 app.get('/', (req, res) => {
   res.send('Hello Node API from JenWebDev')
 })
 
-// Route to fetch/get products from DB
 app.get('/products', async (req, res) => {
   try {
     // holds value of all products in db
@@ -28,7 +25,6 @@ app.get('/products', async (req, res) => {
   }
 })
 
-// Route to fetch/GET single product by id from DB
 app.get('/products/:id', async (req, res) => {
   try {
     //deconstruct id from req.params
@@ -41,7 +37,6 @@ app.get('/products/:id', async (req, res) => {
   }
 })
 
-// Route POST save product to database
 app.post('/product', async (req, res) => {
   try {
     const product = await Product.create(req.body)
@@ -53,40 +48,31 @@ app.post('/product', async (req, res) => {
 })
 
 
-// Route to PUT change single product by id in DB
+
 app.put('/products/:id', async (req, res) => {
   try {
-    //deconstruct id from req.params
     const {id} = req.params;
-
-    // holds value of all products in db- req.body from client
     const product = await Product.findByIdAndUpdate(id, req.body)
-
-    // verify if product exists to update
     if(!product) {
       return res.status(404).json({ message: `cannot find product with ID: ${id}`})
     }
 
-    // if product updated successfully
     res.status(200).json(product)
   }catch(err) {
     res.status(500).json({ message: err.message })
   }
 })
 
-// Remove DELETE product via id
+
 app.delete('/products/:id', async (req, res) => {
   try {
-    //deconstruct id from req.params
+ 
     const {id} = req.params;
-    // find id want to delete
     const product = await Product.findByIdAndDelete(id, req.body)
-    // verify product in database
     if (!product) {
       return res.status(404).json({ message: `cannot find product with ID: ${id}`})
     }
 
-    // if product deleted successfully
     res.status(200).json(product)
   }catch(err) {
     res.status(500).json({ message: err.message })
@@ -94,7 +80,6 @@ app.delete('/products/:id', async (req, res) => {
 })
 
 
-//Connect DB and the server
 mongoose.connect(process.env.DB_STRING)
   .then(()=> {
     console.log(`Connected to mongoDB`)
